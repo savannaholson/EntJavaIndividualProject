ccb112c1da19cfd6abc1c8e3c5494c80920c98dd
savannaholson <banannah@tds.net>
2016-03-19 14:33:37 -0500

    Commiting tons of changes I forgot to commit before.

---
+++ BookTracker/out/artifacts/BookTracker_war_exploded/member/menu-bar.jsp
@@ -0,0 +1,8 @@
+<div id="menu">
+    <a href="index.jsp" id="title">Book Tracker</a>
+
+    <div class="menu-links">
+        <a href="viewBooks.jsp" class="plain-link">My Books</a>
+    </div>
+
+</div>
---
+++ BookTracker/src/entity/UserRolesEntity.java
@@ -0,0 +1,54 @@
+package entity;
+
+import javax.persistence.*;
+
+/**
+ * Created by savannaholson on 3/10/16.
+ */
+@Entity
+@Table(name = "user_roles", schema = "BOOK_TRACKER", catalog = "")
+@IdClass(UserRolesEntityPK.class)
+public class UserRolesEntity {
+    private String username;
+    private String roleName;
+
+    @Id
+    @Column(name = "username")
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Id
+    @Column(name = "role_name")
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        UserRolesEntity that = (UserRolesEntity) o;
+
+        if (username != null ? !username.equals(that.username) : that.username != null) return false;
+        if (roleName != null ? !roleName.equals(that.roleName) : that.roleName != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = username != null ? username.hashCode() : 0;
+        result = 31 * result + (roleName != null ? roleName.hashCode() : 0);
+        return result;
+    }
+}
